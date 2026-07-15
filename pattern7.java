62894c9e0dd5a9f5a6d36a20e7479c2f4c0d7430
sangamithra-ui <24wh1a05e5@bvrithyderabad.edu.in>
2026-07-15 13:31:15 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,24 @@
+package patterns;
+
+public class pattern7 {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+            	
+
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
