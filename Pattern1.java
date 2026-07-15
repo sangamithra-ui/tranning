e5119c5c9ae90808325c269ec5c9e2fbc8499b35
sangamithra-ui <24wh1a05e5@bvrithyderabad.edu.in>
2026-07-15 13:21:44 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N=7;
+		
+		for(int i=1;i<N;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
